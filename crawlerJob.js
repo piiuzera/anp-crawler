eb678cd561c6836faafe46bd58ad1fae46bc0f13
Matheus Amaro <matheus.amaro@gmail.com>
2017-01-08 16:21:04 -0200

    Job running in monday, 00:00:00.

--- crawlerJob.js
+++ crawlerJob.js
@@ -1,6 +1,6 @@
 var schedule = require('node-schedule');
 
-schedule.scheduleJob('* * 0 * * 1', function() {
+schedule.scheduleJob('0 0 0 * * 1', function() {
 	var crawler = require('./crawler');
 
 	console.log('Update Started!');
